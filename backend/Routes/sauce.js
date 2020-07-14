2796c9a1eb9a9d0a3d7c0b4480e246327dbe67f9
Belloir Sébastien <sebastien.belloir@gmail.com>
2020-07-14 10:04:58 +0200

    first commit

---
+++ backend/Routes/sauce.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const sauceController = require('../controllers/sauce');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+router.get('/', auth, sauceController.getAllSauces);
+router.post('/', auth, multer, sauceController.createSauce);
+router.get('/:id', auth, sauceController.getOneSauce);
+router.put('/:id', auth, multer, sauceController.modifySauce);
+router.delete('/:id', auth, sauceController.deleteSauce);
+router.post('/:id/like', auth, sauceController.addLikeAndDislike);
+
+module.exports = router;
---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+const path = require('path');
+
+
+const sauceRoutes = require('./Routes/sauce');
+const userRoutes = require('./Routes/user');
+
+
+mongoose.connect('mongodb+srv://sbelloir:azerty86@cluster0-1qcor.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
