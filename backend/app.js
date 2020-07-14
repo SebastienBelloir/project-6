f032a00b60fd4815158b14846ac6bce5ca402906
Belloir Sébastien <sebastien.belloir@gmail.com>
2020-07-14 10:34:44 +0200

    test

--- backend/app.js
+++ backend/app.js
@@ -28,8 +28,8 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+
 
 module.exports = app;
